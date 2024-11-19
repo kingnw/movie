81d48ee199a3904fe817c466e609ef651ad32629
safal <safalkrki456@gmail.com>
2024-11-19 23:38:02 +1100

    Done

--- finale/ba-master/3002/tests/config.py
+++
@@ -1,5 +0,0 @@
-class TestingConfig:
-    """Configuration for testing environment."""
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
